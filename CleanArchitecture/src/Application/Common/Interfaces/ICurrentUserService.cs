4166ffa1e755da9be54ab332a2fd86d767f145bd
Paras Parmar <parasparmar@gmail.com>
2021-10-03 00:32:56 +0530

    Add project files.

---
+++ CleanArchitecture/src/Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace CleanArchitecture.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+    }
+}
---
+++ CleanArchitecture/tests/Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemTests.cs
@@ -0,0 +1,62 @@
+﻿using CleanArchitecture.Application.Common.Exceptions;
+using CleanArchitecture.Application.TodoItems.Commands.CreateTodoItem;
+using CleanArchitecture.Application.TodoItems.Commands.UpdateTodoItem;
+using CleanArchitecture.Application.TodoLists.Commands.CreateTodoList;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using static Testing;
+
+namespace CleanArchitecture.Application.IntegrationTests.TodoItems.Commands
+{
+    public class UpdateTodoItemTests : TestBase
+    {
+        [Test]
+        public void ShouldRequireValidTodoItemId()
+        {
+            var command = new UpdateTodoItemCommand
+            {
+                Id = 99,
+                Title = "New Title"
+            };
+
+            FluentActions.Invoking(() =>
+                SendAsync(command)).Should().Throw<NotFoundException>();
+        }
+
+        [Test]
+        public async Task ShouldUpdateTodoItem()
+        {
+            var userId = await RunAsDefaultUserAsync();
+
+            var listId = await SendAsync(new CreateTodoListCommand
+            {
+                Title = "New List"
+            });
+
+            var itemId = await SendAsync(new CreateTodoItemCommand
+            {
+                ListId = listId,
+                Title = "New Item"
+            });
+
+            var command = new UpdateTodoItemCommand
+            {
+                Id = itemId,
+                Title = "Updated Item Title"
+            };
+
+            await SendAsync(command);
+
+            var item = await FindAsync<TodoItem>(itemId);
+
+            item.Title.Should().Be(command.Title);
+            item.LastModifiedBy.Should().NotBeNull();
+            item.LastModifiedBy.Should().Be(userId);
+            item.LastModified.Should().NotBeNull();
+            item.LastModified.Should().BeCloseTo(DateTime.Now, 1000);
+        }
+    }
+}
